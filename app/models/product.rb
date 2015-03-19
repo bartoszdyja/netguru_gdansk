21a6b54cd8150e432e6a47720584f3b51c9ec488
Bartosz Dyja <bartosz.dyja@gmail.com>
2015-03-19 14:44:53 +0100

    Added product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,12 @@
 class Product < ActiveRecord::Base
+	validates_presence_of :price, :description, :title
+	validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  def average_rating
+		self.reviews.average(:rating)
+  end
+  
 end
