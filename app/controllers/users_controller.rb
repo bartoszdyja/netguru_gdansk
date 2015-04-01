3eb1849db8a476ab1b5911fe7d262f86d2860494
Bartosz Dyja <bartosz.dyja@gmail.com>
2015-04-01 17:31:19 +0200

    Created user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+	expose :users
+	def show
+	end
+
+	def  index
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 get '/categories/:category_id/products', to: 'categories#show'
+get '/users/', to: 'users#index'
 devise_for :users
   resources :categories do
     resources :products do
